9f61eb9f139324bfc45f261bb4196f839443b3ad
jessicagodin28 <jessica.godin28@gmail.com>
2021-06-24 18:14:03 +0200

    create div js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,7 +1,39 @@
+// promise pour la requete API avec méthode Fetch
+
 fetch("http://localhost:3000/api/teddies")
   .then((res) => {
     return res.json();
   })
-  .then((res) => {
-    console.log(res);
+  .then((teddies) => {
+    console.log(teddies);
+
+    const container_card = document.querySelector(".container_card");
+
+    for (let teddy of teddies) {
+      console.log(teddy.name);
+
+      const aElement = makeElement("a", "");
+      aElement.href = `./product.html?id=${teddy._id}`;
+
+      const divCard = makeElement("div", "card");
+
+      aElement.appendChild(divCard);
+
+      const divCardBody = document.createElement("div");
+      divCardBody.classList.add("card_body");
+
+      divCard.appenChild(divCardBody);
+
+      const divImg = document.createElement("img");
+      divImg.classList.add("card_img_top");
+      divImg.src = teddy.imageUrl;
+      divImg.alt = teddy.name;
+
+      divCardBody.appenChild(divImg);
+      const h3 = document.createElement("h3");
+      h3.innerText = teddy.name;
+
+      divCardBody.appenChild(h3);
+      console.log(divCard);
+    }
   });
